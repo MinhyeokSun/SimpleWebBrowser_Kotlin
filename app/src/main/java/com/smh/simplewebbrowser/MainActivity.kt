52411f6330df75389017ae3183e27b2401768f7a
MinhyeokSun <s.minyeok@gmail.com>
2021-12-20 20:17:37 +0900

    5th Commit

--- app/src/main/java/com/smh/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/smh/simplewebbrowser/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.smh.simplewebbrowser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +35,14 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.webView)
     }
 
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById(R.id.refreshLayout)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -51,6 +63,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true // 구글 메뉴누를수있게해주는 코드
             loadUrl(DEFAULT_URL)  // http는 보안문제가 있지만 웹페이지 구현만 할것이기 때문에 Manifests에 android:usesCleartextTraffic="true" 추가
         }
@@ -63,7 +76,12 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(v.text.toString())
+                val loadingUrl = v.text.toString()
+                if (URLUtil.isNetworkUrl(loadingUrl)) {
+                    webView.loadUrl(v.text.toString())
+                } else {
+                    webView.loadUrl("http://$loadingUrl")
+                }
             }
 
             return@setOnEditorActionListener false
@@ -77,6 +95,38 @@ class MainActivity : AppCompatActivity() {
             webView.goForward()
         }
 
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+
+    }
+
+    inner class WebViewClient : android.webkit.WebViewClient() {  // inner를 쓰는이유는 상위클래스에 접근하기위해서.
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            goBackButton.isEnabled = webView.canGoBack()
+            goFowardButton.isEnabled = webView.canGoForward()
+            addressBar.setText(url)
+        }
+    }
+
+    inner class WebChromeClient : android.webkit.WebChromeClient() {
+
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
+        }
     }
 
     companion object {
