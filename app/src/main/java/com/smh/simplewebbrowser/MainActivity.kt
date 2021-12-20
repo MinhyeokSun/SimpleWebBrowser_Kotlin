99aa3414755f44c64a60e04edeb4f9e2245521d6
MinhyeokSun <s.minyeok@gmail.com>
2021-12-20 18:35:19 +0900

    3rd Commit

--- app/src/main/java/com/smh/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/smh/simplewebbrowser/MainActivity.kt
@@ -7,8 +7,21 @@ import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.goHomeButton)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.goBackButton)
+    }
+
+    private val goFowardButton: ImageButton by lazy {
+        findViewById(R.id.goFowardButton)
+    }
 
     private val addressBar: EditText by lazy {
         findViewById(R.id.addressBar)
@@ -26,16 +39,28 @@ class MainActivity : AppCompatActivity() {
         bindViews()
     }
 
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     @SuppressLint("SetJavaScriptEnabled") // 보안경고무시
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
             settings.javaScriptEnabled = true // 구글 메뉴누를수있게해주는 코드
-            loadUrl("http://www.google.com")  // http는 보안문제가 있지만 웹페이지 구현만 할것이기 때문에 Manifests에 android:usesCleartextTraffic="true" 추가
+            loadUrl(DEFAULT_URL)  // http는 보안문제가 있지만 웹페이지 구현만 할것이기 때문에 Manifests에 android:usesCleartextTraffic="true" 추가
         }
     }
 
     private fun bindViews() {
+        goHomeButton.setOnClickListener {
+            webView.loadUrl(DEFAULT_URL) // 되긴 되는데 기본주소를 바꿀경우 2번바꿔야함. 따라서 별도 함수로 빼야함.
+        }
+
         addressBar.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 webView.loadUrl(v.text.toString())
@@ -43,6 +68,19 @@ class MainActivity : AppCompatActivity() {
 
             return@setOnEditorActionListener false
         }
+
+        goBackButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        goFowardButton.setOnClickListener {
+            webView.goForward()
+        }
+
+    }
+
+    companion object {
+        private const val DEFAULT_URL = "http://www.google.com"
     }
 
 }
